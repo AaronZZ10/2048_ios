41c1c2c8356b14348f844448d8ebcad90ef50a52
Aaron Liu <aaronzz10101@gmail.com>
2025-11-08 14:44:42 -0600

    Initial Commit

---
+++ My2048Game/My2048GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  My2048GameApp.swift
+//  My2048Game
+//
+//  Created by Hao Liu on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct My2048GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
